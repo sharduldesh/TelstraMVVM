e85043bd61899d7c17f9d2200624a5e069a9fa42
sharduldesh <63037937+sharduldesh@users.noreply.github.com>
2020-05-07 14:40:54 +0530

    Delete CountryInfoModelData.kt

--- app/src/main/java/com/example/telstraproficiency/model/CountryInfoModelData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.telstraproficiency.data.model
-
-/**Data class for information**/
-data class CountryInfoModelData(
-    var title: String,
-    var description: String,
-    var imageHref: String
-)
