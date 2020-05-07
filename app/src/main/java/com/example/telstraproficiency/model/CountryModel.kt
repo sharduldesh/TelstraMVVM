a17ccb4dfd42beb1a9418ad9a53f6953cc2e42ed
sharduldesh <63037937+sharduldesh@users.noreply.github.com>
2020-05-07 14:41:08 +0530

    Delete CountryModel.kt

--- app/src/main/java/com/example/telstraproficiency/model/CountryModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.telstraproficiency.data.model
-
-/**Model Class for JSON**/
-class CountryModel
-{
-    var title:String=""
-    lateinit var rows: ArrayList<CountryInfoModelData>
-}
