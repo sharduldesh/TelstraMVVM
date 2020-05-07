2ca6b3f0683103b5fcbe77d7d6cb0c877b6ce336
sharduldesh <63037937+sharduldesh@users.noreply.github.com>
2020-05-07 14:42:10 +0530

    data classes added
    added model data class for response json

---
+++ app/src/main/java/com/example/telstraproficiency/data/model/CountryInfoModelData.kt
@@ -0,0 +1,8 @@
+package com.example.telstraproficiency.data.model
+
+/**Data class for information**/
+data class CountryInfoModelData(
+    var title: String,
+    var description: String,
+    var imageHref: String
+)
---
+++ app/src/main/java/com/example/telstraproficiency/data/model/CountryModel.kt
@@ -0,0 +1,8 @@
+package com.example.telstraproficiency.data.model
+
+/**Model Class for JSON**/
+class CountryModel
+{
+    var title:String=""
+    lateinit var rows: ArrayList<CountryInfoModelData>
+}
