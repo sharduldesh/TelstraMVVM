a48a98d9d13a8fb8d2672563f58803c655e032e6
sharduldesh <63037937+sharduldesh@users.noreply.github.com>
2020-05-08 16:09:30 +0530

    more specific constant variable name
    more specific constant variable name used for error message

--- app/src/main/java/com/example/telstraproficiency/utils/Constant.kt
+++ app/src/main/java/com/example/telstraproficiency/utils/Constant.kt
@@ -3,7 +3,7 @@ package com.example.telstraproficiency.utils
 class Constant
 {
     companion object {
-       const val somethingWentWrong="Something went wrong"
+       const val unknownError="Something went wrong"
        const val countryUrl :String = "/s/2iodh4vg0eortkl/facts.json"
     }
 }
